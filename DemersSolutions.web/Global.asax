e660d5b88e4e99bc0d3e084e84a7f2a894eb6194
OneQuickOne <northof490@gmail.com>
2014-08-29 17:22:12 -0700

    Initial Check in

---
+++ DemersSolutions.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemersSolutions.web.MvcApplication" Language="C#" %>
---
+++ DemersSolutions.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemersSolutions.web.Startup))]
+namespace DemersSolutions.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
