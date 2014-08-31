d106667d6561ec43b72e90245a71240063374917
OneQuickOne <northof490@gmail.com>
2014-08-31 14:08:22 -0700

    Removed unused layers

--- DemersSolutions.web/Controllers/HomeController.cs
+++ DemersSolutions.web/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 namespace DemersSolutions.web.Controllers {
     public class HomeController : Controller {
         public ActionResult Index() {
+
             return View("Index");
         }
 
